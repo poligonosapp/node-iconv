a15fe24ac41258426112e10877ee50085c4fb180
Luis Mendes <machadoluismendes@gmail.com>
2021-07-10 21:03:27 -0300

    nodejs encoding
    https://github.com/nodejs/string_decoder

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
-import $ from "jquery"; 
+import $ from "jquery";
+// https://github.com/nodejs/string_decoder
+// https://github.com/nodejs/readable-stream
+import { Readable, Writable, Transform, Duplex, pipeline, finished } from 'readable-stream';
+
+import { StringDecoder } from 'string_decoder';
+const decoder = new StringDecoder('utf8');
+
+decoder.write(Buffer.from([0xe2]));
+decoder.write(Buffer.from([0x82]));
+console.log(decoder.end(Buffer.from([0xac])));
+
+export { Readable, Writable, Transform, Duplex, pipeline, finished };
+    
+
+
+
 
---
+++ webpack.config.ts
@@ -0,0 +1,14 @@
+const webpack = require('webpack');
+
+module.exports = {
+    plugins: [
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
+    ],
+    resolve: {
+        fallback: {
+            buffer: require.resolve('buffer/'),
+        },
+    },
+}
